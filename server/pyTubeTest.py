a06242857fcd775820de80c8e01b26aea9b95fbd
justnat3 <nd2010@reedusa.com>
2020-10-04 17:22:19 -0500

    Added return value outside of progrm, Added State for pass or fail using enum

--- server/pyTubeTest.py
+++ server/pyTubeTest.py
@@ -3,11 +3,16 @@ import os
 import pytube
 from halo import Halo
 from pathlib import Path
-
+from enum import Enum
 
 """VARS"""
-url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+#url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 
+url = "https://www.youtube.com/watch?v=dQw4w9"
+class State:
+
+    PATH_STATE: bool = True
+    DOWNLOAD_STATE: bool = True
 
 class Test_Suite:
 
@@ -31,8 +36,8 @@ class Test_Suite:
         """
 
         if self.win_path and self.linux_path == None:
+            State.PATH_STATE = 0
             raise AssertionError("No Path Found")
-            exit(0)
         elif self.win_path != None:
             home_dir = self.win_path
             self.spinner.succeed(text="Windows Path Detected")
@@ -99,12 +104,12 @@ class Test_Suite:
         except:
             self.spinner.fail(text="Could not reach URL -> FAIL\n")
             self.spinner.stop()
-            exit(0)
+            State.DOWNLOAD_STATE = 0
 
         if os.path.isfile(a):
             self.spinner.fail(text="File Exists")
             self.spinner.stop()
-            exit(1)
+            State.PATH_STATE = 0
 
         """for testing purposes we do not care about resolution
             for this case we will use a low-pixel res for effeciency
@@ -121,23 +126,43 @@ class Test_Suite:
         except:
             self.spinner.fail(text="Test Failed because video download -> FAILED\n")
             self.spinner.stop()
-            exit(0)
+            State.DOWNLOAD_STATE = 0
         if os.path.isfile(a):
             self.spinner.succeed(text="Video Download -> PASS")
             self.spinner.start()
         else:
             self.spinner.fail(text="Download Failed -> file does not exist\n")
             self.spinner.stop()
-            exit(1)
+            State.DOWNLOAD_STATE = 0
 
         self.spinner.start()
         self.spinner.stop()
 
-        print("\nTesting Finished...")
+        # Check if anything fails for CI
+        if State.DOWNLOAD_STATE == 0:
+            print("")
+            self.spinner.info(text="Tests Failed")
+            self.spinner.stop()
+            print("")
+            return False
+        elif State.PATH_STATE == 0:
+            print("")
+            self.spinner.info(text="Tests Failed")
+            self.spinner.stop()
+            print("")
+            return False
+        else:
+            print("")
+            self.spinner.info(text="Tests Passed")
+            self.spinner.stop()
+            print("")
+            return True
 
 
 if __name__ == "__main__":
     suite = Test_Suite()
     suite.GetDownloadsFolder()
     home_dir = suite.WhoAmI()
-    suite.Test(url, home_dir)
+    r = suite.Test(url, home_dir)
+    if r == False:
+        exit(1)
